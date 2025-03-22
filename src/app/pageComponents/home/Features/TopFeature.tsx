a55a64c32cca40f6312b4dca47ea81fea960c2f8
Neermal1 <neermaldongal4@gmail.com>
2025-03-22 10:09:02 +0545

    build

--- src/app/pageComponents/home/Features/TopFeature.tsx
+++ src/app/pageComponents/home/Features/TopFeature.tsx
@@ -4,24 +4,29 @@ import ComponentHeader from "@/app/components/componentHeader/ComponentHeader";
 import { ITopFeature } from "@/app/interface/interface";
 import { motion } from "framer-motion";
 import Lottie from "lottie-web";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import feature from "../../../../../public/animation/json/features_json.json";
 
 const TopFeature = ({ feature_data }: ITopFeature) => {
-  const featureReference: any = useRef(null);
+  const featureReference = useRef<HTMLDivElement | null>(null);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    const animation = Lottie.loadAnimation({
-      container: featureReference.current,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: feature,
-    });
+    setIsClient(true);
 
-    return () => {
-      animation.destroy();
-    };
+    if (typeof window !== "undefined" && featureReference.current) {
+      const animation = Lottie.loadAnimation({
+        container: featureReference.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: feature,
+      });
+
+      return () => {
+        animation.destroy();
+      };
+    }
   }, []);
 
   return (
@@ -31,19 +36,13 @@ const TopFeature = ({ feature_data }: ITopFeature) => {
         whileInView="whileInView"
         className="grid lg:grid-cols-12 gap-[120px] items-start"
       >
-        <motion.div
-          // variants={left_animation}
-          // variants={left_animation(isMobile)}
-          className="lg:col-span-5  rounded-md"
-        >
+        <motion.div className="lg:col-span-5 rounded-md">
           <div className="relative">
-            <div ref={featureReference}></div>
+            {isClient && <div ref={featureReference}></div>}
           </div>
         </motion.div>
-        <motion.div
-          // variants={right_animation(isMobile)}
-          className="lg:col-span-7 flex flex-col gap-10"
-        >
+
+        <motion.div className="lg:col-span-7 flex flex-col gap-10">
           <div className="flex flex-col gap-4">
             <div className="flex flex-col gap-10">
               <ComponentHeader
@@ -57,49 +56,45 @@ const TopFeature = ({ feature_data }: ITopFeature) => {
               It covers essential steps such as market research, business
               planning, securing funding, and navigating the challenges of
               startup growth, providing practical advice and real-world examples
-              to inspire and equip readers for entrepreneurial success
+              to inspire and equip readers for entrepreneurial success.
             </div>
           </div>
-          <div className="flex flex-col gap-10">
-            {feature_data?.map((data, index) => {
-              return (
-                <div key={data?.id}>
-                  <motion.div
-                    initial={{
-                      y: 10,
-                      opacity: 0,
-                    }}
-                    whileInView={{
-                      y: 0,
-                      opacity: 1,
 
-                      transition: {
-                        delay: 0.4 * data?.id,
-                        duration: 0.8,
-                        staggerChildren: 0.8,
-                        type: "spring",
-                        stiffness: 600,
-                      },
-                    }}
-                    className="flex items-start gap-6 border-b-[2px] border-gray-600 pb-3"
-                  >
-                    <div className="mt-[1px] text-gray-600 opacity-50 lg:text-[25px] font-semibold">
-                      0{index + 1}
+          <div className="flex flex-col gap-10">
+            {feature_data?.map((data, index) => (
+              <div key={data?.id}>
+                <motion.div
+                  initial={{ y: 10, opacity: 0 }}
+                  whileInView={{
+                    y: 0,
+                    opacity: 1,
+                    transition: {
+                      delay: 0.4 * data?.id,
+                      duration: 0.8,
+                      staggerChildren: 0.8,
+                      type: "spring",
+                      stiffness: 600,
+                    },
+                  }}
+                  className="flex items-start gap-6 border-b-[2px] border-gray-600 pb-3"
+                >
+                  <div className="mt-[1px] text-gray-600 opacity-50 lg:text-[25px] font-semibold">
+                    0{index + 1}
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="lg:text-[22px] text-[20px] font-bold">
+                      {data?.title}
                     </div>
-                    <div className="flex flex-col gap-2">
-                      <div className="lg:text-[22px] text-[20px] font-bold">
-                        {data?.title}
-                      </div>
-                      <div className="text-gray-600 opacity-50">
-                        {data?.description}
-                      </div>
+                    <div className="text-gray-600 opacity-50">
+                      {data?.description}
                     </div>
-                  </motion.div>
-                </div>
-              );
-            })}
+                  </div>
+                </motion.div>
+              </div>
+            ))}
           </div>
-          <div className="">
+
+          <div>
             <PrimaryButton>Get Started</PrimaryButton>
           </div>
         </motion.div>
